0bedecd690c1f675cae8906ab70b6cacd95c72af
TCarey03 <tylercarey03@gmail.com>
2026-09-20 11:03:35 -0500

    Phase 1 completed.

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BradyBunchApp());
+}
+
+class BradyBunchApp extends StatelessWidget {
+  const BradyBunchApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: BradyTile(
+          imagePath: 'assets/images/alice.jpg',
+        ),
+      ),
+    );
+  }
+}
+
+class BradyTile extends StatelessWidget {
+  final String imagePath;
+
+  const BradyTile({
+    super.key,
+    required this.imagePath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(4.0),
+      color: Colors.black,
+      child: Image.asset(
+        imagePath,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
